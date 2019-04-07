12a5b80e2039782321501e8b3f177d4d1e345122
emrido <dodooridoo@gmail.com>
2019-04-08 03:13:33 +0700

    server client created

---
+++ server/controller/user.js
@@ -0,0 +1,104 @@
+const User = require('../models/user');
+const { decrypt } = require('../helpers/bcrypt');
+const jwt = require('jsonwebtoken');
+const { OAuth2Client } = require('google-auth-library');
+const CLIENT_ID = process.env.CLIENT_ID
+const client = new OAuth2Client(CLIENT_ID);
+
+class UserController {
+    static register(req, res) {
+        User
+            .create(req.body)
+            .then(newUser => {
+                res.status(201).json(newUser);
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: err.message
+                })
+            })
+    }
+
+    static login(req, res) {
+        User
+            .findOne({
+                email: req.body.email
+            })
+            .then(foundUser => {
+                if (!foundUser) {
+                    res.status(404).json({
+                        message: 'User not found'
+                    })
+                } else {
+                    if (decrypt(req.body.password, foundUser.password)) {
+                        const token = jwt.sign(
+                            {
+                                _id: foundUser._id,
+                                name: foundUser.name
+                            }
+                        , process.env.JWT_SECRET);
+
+                        res.status(200).json(token);
+                    } else {
+                        res.status(401).json({
+                            message: 'Wrong password'
+                        });
+                    }
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: err.message
+                })
+            })
+    }
+
+    static verify(req, res) {
+        let payload;
+        let token;
+
+        client
+            .verifyIdToken({
+                idToken: req.body.token,
+                audience: CLIENT_ID,
+            })
+            .then(ticket => {
+                payload = ticket.getPayload();
+                const userid = payload['sub'];
+
+                return User.findOne({ 
+                    email: payload.email
+                })
+            })
+            .then(user => {
+                if (!user) {
+                    return User.create(
+                        { 
+                            name: payload.name,
+                            email: payload.email,
+                            password: qwascvnl123
+                        }
+                    )
+                } else {
+                    return user
+                }
+            })
+            .then(newUser => {
+                token = jwt.sign(
+                    { 
+                        id: newUser._id,
+                        name: newUser.name 
+                    }
+                , process.env.JWT_SECRET)
+
+                res.status(200).json(token)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: err.message
+                })
+            })
+    }
+}
+
+module.exports = UserController;
---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const userRoute = require('./user');
+const todoRoute = require('./todo');
+const { authentication } = require('../middlewares/authentication');
+
+router.use('/users', userRoute);
+router.use('/todos', authentication, todoRoute);
+
+module.exports = router;
